4405ecd597bb59d1043df31e6dbd9e0fd1db351b
lastrod1 <mtran3938@gmail.com>
2026-07-04 12:51:50 -0500

    Completed line detection using probabilistic_hough

--- Sudoku_Solver/src/grid_detection/pipeline.py
+++ Sudoku_Solver/src/grid_detection/pipeline.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 from preprocessing import preprocessing
+from probabilistic_hough import probabilistic_hough, draw_lines
 
 # Testing of image processinging
 RAW_IMAGE_DIR = "../../images/raw_images/"
@@ -11,3 +12,12 @@ IMAGE = RAW_IMAGE_DIR + RAW_IMAGE_FILE
 image = cv.imread(IMAGE)
 preprocessed = preprocessing(image)
 cv.imwrite(f"{TEST_IMAGE_DIR}preprocessing.png", preprocessed)
+
+_, lines = probabilistic_hough(preprocessed)
+
+blank_canvas = np.zeros(image.shape, dtype=np.uint8)
+lines_image = draw_lines(blank_canvas, lines)
+
+cv.imwrite(f"{TEST_IMAGE_DIR}lines_image.png", lines_image)
+
+print (lines)
---
+++ Sudoku_Solver/src/grid_detection/probabilistic_hough.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+
+def draw_lines(image, lines):
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv.line(image, (x1, y1), (x2, y2), (255,0,0), 3)
+
+    return image
+
+def probabilistic_hough(image):
+    # should take in the binary image
+    MINLINELENGTH = image.shape[1] // 4
+    edges = cv.Canny(image, 120, 255)
+    lines = cv.HoughLinesP(edges, 1, np.pi/180, 68, minLineLength=MINLINELENGTH, maxLineGap=20)
+
+    image = draw_lines(image, lines)
+
+    return image, lines
+
+
